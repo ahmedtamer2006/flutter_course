65d8d30dd6922bbd748b77005e6b3b10ccb9dc5f
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-18 17:36:45 +0300

    feat: Update main.dart with new color schemes and improve app bar title; add launch.json for debugging configurations

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: (Text("Ahmed Tamer"))),
+        appBar: AppBar(title: (Text("Ahmed Tamer pro"))),
         body: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
             children: [
               Container(color: Colors.red, width: 100, height: 100),
-              Container(color: Colors.green, width: 100, height: 100),
+              Container(
+                color: const Color.fromARGB(255, 12, 12, 12),
+                width: 100,
+                height: 100,
+              ),
               Container(color: Colors.black, width: 100, height: 100),
               Container(color: Colors.yellow, width: 100, height: 100),
               Container(color: Colors.red, width: 100, height: 100),
               Container(color: Colors.green, width: 100, height: 100),
-              Container(color: Colors.black, width: 100, height: 100),
-              Container(color: Colors.yellow, width: 100, height: 100),
+              Container(
+                color: const Color.fromARGB(9, 0, 0, 0),
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: const Color.fromARGB(255, 166, 161, 116),
+                width: 100,
+                height: 100,
+              ),
             ],
           ),
         ),
