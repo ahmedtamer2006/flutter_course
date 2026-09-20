fc830bc7fdcfddb21f7f5ba0765d7c014ed61902
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-20 22:49:03 +0300

    feat: Implement basic UI structure in MyApp with AppBar and styled Text widget

--- lib/container_widget.dart
+++ lib/container_widget.dart
@@ -9,6 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Subjects Online")),
+        body: Text(
+          "Accounting",
+          style: TextStyle(
+            color: Colors.deepOrange,
+            fontSize: 40,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/text_widget.dart
+++ lib/text_widget.dart
@@ -9,6 +9,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Subjects Online")),
+        body: Text(
+          "Accounting",
+          style: TextStyle(
+            color: Colors.deepOrange,
+            fontSize: 40,
+            fontWeight: FontWeight.w600,
+            // backgroundColor: Colors.pink,
+            letterSpacing: 2,
+            wordSpacing: 10,
+            height: 1.5,
+            background: Paint()..color = const Color.fromARGB(255, 166, 53, 70),
+          ),
+        ),
+      ),
+    );
   }
 }
